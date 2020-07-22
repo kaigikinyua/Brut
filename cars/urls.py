405857d9199818fa847b61c8111b1b922c565d75
kaigikinyua <kaigikinyua@gmail.com>
2020-07-22 22:32:49 +0300

    getting car details - not tested

--- cars/urls.py
+++ cars/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('listCars',views.listCars,name="listCars")
+    path('listCars',views.listCars,name="listCars"),
+    path('getCarDetails<str:model>',views.getCarDetails,name="getCarDetails")
 ]
--- cars/views.py
+++ cars/views.py
@@ -9,3 +9,8 @@ def listCars(request):
         print(car.car_model)
         cars_json.append({"car_model":car.car_model,"door_num":car.door_no,"seats":car.seat_no,"desc":car.description,"year_of_manu":car.year_of_man,"class":car.car_class,"image":"http://localhost:8080/carpics/2.jpg"})
     return JsonResponse({"state":"ok","cars":cars_json})
+
+def getCarDetails(request,carmodel):
+    cars=Car.objects.filter(car_model=carmodel)
+    car_photos=Car.objects.filter(cars)
+    return JsonResponse({"state":"ok","car":cars,"car_photos":car_photos})
