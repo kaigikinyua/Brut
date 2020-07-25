cfd3ddd9bd79a65026d56c12bd9923ec133ea158
kaigikinyua <kaigikinyua@gmail.com>
2020-07-25 23:04:02 +0300

    superuser action model

--- reception/models.py
+++ reception/models.py
@@ -6,3 +6,8 @@ class SuperUser(models.Model):
     password=models.CharField(max_length=50)
     email=models.CharField(max_length=100)
     token=models.CharField(max_length=50)
+
+class Action(models.Moldel):
+    token=models.CharField(max_length=50)
+    action=models.CharField(max_length=50)
+    dot=models.DateTimeField(auto_now=False, auto_now_add=False)
