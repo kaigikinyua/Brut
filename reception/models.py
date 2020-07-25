828d3b38503c6015679dae894f5774857fa60294
kaigikinyua <kaigikinyua@gmail.com>
2020-07-25 22:59:48 +0300

    super user model

--- reception/models.py
+++ reception/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SuperUser(models.Model):
+    username=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
+    email=models.CharField(max_length=100)
+    token=models.CharField(max_length=50)
